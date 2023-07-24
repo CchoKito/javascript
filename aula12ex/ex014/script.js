3081d42f4014f9062636feef79598aa16d28c215
Yan Carlos <88597810+CchoKito@users.noreply.github.com>
2023-07-24 10:47:12 -0300

    parte 2 do site

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,24 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 19
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // Bom dia!
+        img.src = 'sol-da-manha.jpg'
+        document.body.style.background = '#0075CD'
+    } else if (hora >= 12 && hora <= 18) {
+        // Boa tarde!
+        img.src = 'sol-da-tarde.jpg'
+        document.body.style.background = '#CF6803'
+    } else {
+        // Boa noite!
+        img.src = 'sol-da-noite.jpg'
+        document.body.style.background = '#003E3A'
+    }
+}
+
+// colocar um gradiente
+// colocar fotos melhores ou em 4K
